cc62b1323a96355ac5fbce9ae9ecee01cfbdf33a
shagun Sodhani <sshagunsodhani@gmail.com>
2016-09-21 16:27:19 +0530

    Added basic script to scrape reddit posts

---
+++ app/scrape.py
@@ -0,0 +1,28 @@
+import praw
+
+
+def read_subreddit_list():
+    subreddit_list_path = '/home/shagun/projects/reddit-text/config/subreddit.txt'
+    subreddit_limit_list = []
+    with open(subreddit_list_path) as f:
+        for _line in f:
+            line = _line.strip().split()
+            subreddit = line[0]
+            if (len(line) > 1 and line[1]):
+                limit = line[1]
+            else:
+                limit = None
+            subreddit_limit_list.append((subreddit, limit))
+    return subreddit_limit_list
+
+
+reddit = praw.Reddit(user_agent='dummy')
+
+for subreddit, limit in read_subreddit_list():
+    for submission in reddit.get_subreddit(subreddit).get_hot(limit=limit):
+        if (submission.selftext):
+            text = submission.title + "\n" + submission.selftext
+            print(text)
+        else:
+            text = submission.url
+            print("url")
