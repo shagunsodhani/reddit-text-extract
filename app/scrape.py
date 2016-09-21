779bf2abc4d7250199b366b970f7e6c9d7c0ca93
shagun Sodhani <sshagunsodhani@gmail.com>
2016-09-21 16:54:05 +0530

    wrapped the logic to scrape reddit in a function

--- app/scrape.py
+++ app/scrape.py
@@ -1,8 +1,11 @@
+from time import time
+
 import praw
 
 from config import parse_config
 
 config = parse_config("local")
+
 
 def read_subreddit_list():
     subreddit_list_path = config['subreddit_list_path']
@@ -19,16 +22,33 @@ def read_subreddit_list():
     return subreddit_limit_list
 
 
-reddit = praw.Reddit(user_agent=config['user_agent'])
+def scrape_reddit_text():
+    file_to_save_to = config['path_to_save']
+    start_time = time()
+    counter = 0
+    with open(file_to_save_to, 'wb') as f:
+        reddit = praw.Reddit(user_agent=config['user_agent'])
+        for subreddit, limit in read_subreddit_list():
+            text = ''
+            for submission in reddit.get_subreddit(subreddit).get_hot(limit=limit):
+                counter += 1
+                if (counter % 1000 == 0):
+                    end_time = time()
+                    print(str(counter) + " number of submissions parsed in "
+                          + str(end_time - start_time) + " seconds.")
+                    start_time = end_time
+                if (submission.selftext):
+                    text = submission.title + "\n" + submission.selftext
+                    print(text)
+                else:
+                    text = submission.url
+                    print("url")
+                for comment in submission.comments:
+                    text += "\n" + comment.body
+                print(text)
 
-for subreddit, limit in read_subreddit_list():
-    for submission in reddit.get_subreddit(subreddit).get_hot(limit=limit):
-        if (submission.selftext):
-            text = submission.title + "\n" + submission.selftext
-            print(text)
-        else:
-            text = submission.url
-            print("url")
-        for comment in submission.comments:
-            text += "\n" + comment.body
-        print(text)
+            f.write(text)
+
+
+if __name__ == "__main__":
+    scrape_reddit_text()
